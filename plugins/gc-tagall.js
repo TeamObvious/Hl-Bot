c31cc19ad957eca4fd1fd790b484cdad39f9216a
team obvious <149536321+TeamObvious@users.noreply.github.com>
2023-11-27 16:39:30 -1200

    ✿ㅤ𝐇𝐋 𝐁𝐎𝐓ㅤ✿

--- plugins/gc-tagall.js
+++ plugins/gc-tagall.js
@@ -6,11 +6,11 @@ const handler = async (m, {isOwner, isAdmin, conn, text, participants, args, com
   }
   const pesan = args.join` `;
   const oi = `𝙎𝙞 𝘿𝙚𝙨𝙚𝙖𝙨 𝙀𝙡 𝘽𝙤𝙩 𝘾𝙤𝙢𝙥𝙧𝙖 𝘾𝙤𝙣 wa.me/51906528530 ${pesan}`;
-  let teks = `𝙀𝙮 𝙈𝙖𝙡𝙙𝙞𝙩𝙤 𝙉𝙚𝙜𝙧𝙭 𝙏𝙚 𝘼𝙣𝙤𝙩𝙖𝙨 𝙊 𝙏𝙚 𝙎𝙖𝙘𝙤 𝙃𝙞𝙟𝙭 𝘿𝙚 𝙋𝙪𝙩𝙖 \n\n ${oi}\n\n`;
+  let teks = `𝐀𝐩𝐚𝐫𝐞𝐜𝐞 𝐞𝐥 𝐮𝐧𝐢𝐜𝐨 𝐛𝐨𝐭 𝐚𝐪𝐮𝐢 𝐬𝐨𝐲 𝐲𝐨 𝐲 𝐥𝐚 𝐪𝐮𝐞𝐬𝐨. 𝐏𝐮𝐫𝐨 𝐇𝐋💋🌸 \n\n ${oi}\n\n`;
   for (const mem of participants) {
-    teks += `🦂| ▪ @${mem.id.split('@')[0]}\n`;
+    teks += `🌸| 💋 @${mem.id.split('@')[0]}\n`;
   }
-  teks += `\n\n🦂 𝘼𝙡𝙖𝙘𝙧𝙖𝙣 🦂`;
+  teks += `\n\n✿ㅤ𝐇𝐋 𝐁𝐎𝐓ㅤ✿`;
   conn.sendMessage(m.chat, {text: teks, mentions: participants.map((a) => a.id)} );
 };
 handler.help = ['tagall <mesaje>', 'invocar <mesaje>'];
